3556f8ca336a79da82e721b9bb8d5642608960d9
Shalu08 <shaluch123chauhan@gmail.com>
2019-11-13 12:57:31 +0530

    some

--- app/src/main/java/myapp/Akka/akkacircle/Activity/SignUp.java
+++ app/src/main/java/myapp/Akka/akkacircle/Activity/SignUp.java
@@ -16,6 +16,7 @@ public class SignUp extends AppCompatActivity {
     private Button _loginbtn;
     private EditText _name,_email,_phone,_password;
     private TextView _loginnow;
+    
     private CheckBox checkBox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
